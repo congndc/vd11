73c87e2a35fb78ea1f27207e1eaadb4c23751237
nguyenduycongndc <nguyenduycongndc@gmail.com>
2020-02-27 17:32:05 +0700

    5.31.27.2

---
+++ vd11/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using vd11.Models;
+
+namespace vd11.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult AddOrEdit(int ID=0)
+        {
+            User user = new User();
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(User user)
+        {
+            using (DbModel)
+        }
+    }
+}
---
+++ vd11/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vd11.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+       
+       
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage ="Lỗi Mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Lỗi Password")]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Lỗi ConfirmPassword")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Lỗi IsPassword")]
+        public bool IsPassword { get; set; }
+    }
+}
